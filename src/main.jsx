45cb62d481e49b745ccd99ebae2f7284d3ebf407
Jebeh <131396477+jkawah@users.noreply.github.com>
2026-06-29 22:34:40 +0000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import KawahSystems from "./KawahSystems.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <KawahSystems />
+  </React.StrictMode>
+);
